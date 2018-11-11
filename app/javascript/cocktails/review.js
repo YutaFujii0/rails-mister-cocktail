301e029c191a735f776d1c4cc2423fab6d7bf520
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-11 11:32:54 +0900

    Create review date feature

---
+++ app/javascript/cocktails/review.js
@@ -0,0 +1,10 @@
+
+const url = "https://barful-life-187.herokuapp.com/cocktails/2";
+
+const updatePost = () => {
+  fetch(url)
+  .then(response = response.json())
+  .then((data) => {
+
+  });
+};
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,17 @@ class Review < ApplicationRecord
   # validates :name, length: {max: 15}
   validates :content, presence: true
   validates :rating, presence: true
+
+  def days_ago
+    min_ago = ((Time.now - created_at) / 60).round
+    if min_ago < 60
+      "#{min_ago} min ago"
+    elsif min_ago / 60 < 24
+      hour_ago = min_ago / 60
+      "#{hour_ago} #{'hour'.pluralize(hour_ago)} ago"
+    else
+      day_ago = min_ago / 60 / 24
+      "#{day_ago} #{'day'.pluralize(day_ago)} ago"
+    end
+  end
 end
